5b47dce5983789b858835a7e321316357e548c20
Sebastian <seborbon@outlook.com>
2022-02-09 01:43:41 -0500

    Home Created

--- Front/src/pages/home/Home.jsx
+++ Front/src/pages/home/Home.jsx
@@ -1,16 +1,37 @@
 import styled from "styled-components";
 import SideBar from "../../components/sideBar/SideBar";
 import ToolBar from "../../components/toolbar/ToolBar";
+import backgroundImg from "../../images/BgHome.jpg";
 
 const Container = styled.div`
   background-color: white;
   display: flex;
 `;
+
+const Wrapper = styled.div`
+
+  width 100%;
+`;
+const ImageContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-left: 40px;
+  margin-top: 40px;
+`;
+const Image = styled.img`
+  width: 80%;
+`;
 const Home = () => {
   return (
     <Container>
       <SideBar />
-      <ToolBar />
+      <Wrapper>
+        <ToolBar />
+        <ImageContainer>
+          <Image src={backgroundImg}></Image>
+        </ImageContainer>
+      </Wrapper>
     </Container>
   );
 };
